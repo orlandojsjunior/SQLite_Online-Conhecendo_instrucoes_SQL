0f773a1bdcd7245e1613eb2edd1e8f7fbc4b5dee
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-29 12:06:20 -0300

    Create 02 - Identificando os clientes que se cadastraram antes de 2020.sql

---
+++ Exercicios Aula 1 -  Atividade 10 - Mão na massa - aplicando na prática/02 - Identificando os clientes que se cadastraram antes de 2020.sql
@@ -0,0 +1,10 @@
+-- Identificando os clientes que se cadastraram antes de 2020
+
+SELECT nome_do_cliente, data_de_cadastro
+FROM tabela_clientesdesafio_aula1
+WHERE data_de_cadastro < '2020-01-01'; 
+
+
+
+
+
