46bd47602fc4749f376f773befe1c5e7af5872a2
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-29 12:58:32 -0300

    Aula 2 - Atividade 5 Alterando e excluindo tabelas

---
+++ Aula 2 - Atividade 5 Alterando e excluindo tabelas.sql
@@ -0,0 +1,8 @@
+SELECT * FROM tabelaclientes;
+
+ALTER TABLE tabelaclientes ADD Endereço_Cliente VARCHAR (250)  -- alterando tabela, adicionando uma coluna.
+
+
+
+DROP TABLE tabelaclientes; -- Excluido uma tabela específica.
+
