7df32af18dcf79c98719e8bbb6c04d048e14f1a0
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-30 19:12:22 -0300

    Aula 2 - Atividade 8 Chave primária - PRIMARY KEY

---
+++ Aula 2 - Atividade 10 Chave estrangeira - FOREIGN KEY.sql
@@ -0,0 +1,24 @@
+CREATE TABLE tabelaprodutos (
+  ID_Produto INT PRIMARY KEY,
+  Nome_do_Produto VARCHAR (250),
+  Descrição TEXT,
+  Categoria INT,
+  Preco_de_Compra DECIMAL (10,2),
+  Unidade VARCHAR (50),
+  Fornecedor INT,
+  Data_de_Inclusao DATE
+);
+
+
+CREATE TABLE tabelaprodutos (
+  ID_Produto INT PRIMARY KEY,
+  Nome_do_Produto VARCHAR (250),
+  Descrição TEXT,
+  Categoria INT,
+  Preco_de_Compra DECIMAL (10,2),
+  Unidade VARCHAR (50),
+  Fornecedor INT,
+  Data_de_Inclusao DATE,
+  FOREIGN KEY (Categoria) REFERENCES tabelacategorias (id_categoria),
+  FOREIGN KEY (Fornecedor) REFERENCES tabelafornecedores (id)
+);
