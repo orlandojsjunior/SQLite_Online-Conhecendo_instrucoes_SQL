627cedee2f91752095688820fd7f9d530a8610fb
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-01 16:44:21 -0300

    Aula 3 - Atividade 03 Inserindo múltiplas linhas

---
+++ Aula 3 - Atividade 03 Inserindo múltiplas linhas.sql
@@ -0,0 +1,11 @@
+INSERT INTO tabelaclientes
+(id_cliente,
+nome_cliente,
+informacoes_de_contato,
+Endereço_Cliente)
+VALUES
+('2', 'João Santos', 'joao.santos@provedor.com', 'Rua dos pinheiros, 25'),
+('3', 'Maria Fernandes', 'maria.fernandes@email.com', 'Rua Santo Antonio, 10'),
+('4', 'Carlos Pereira', 'carlos.pereira@email.com', 'Avenida rio, 67');
+
+SELECT * FROM TABELACLIENTES;
