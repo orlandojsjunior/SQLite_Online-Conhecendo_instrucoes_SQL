ff30497f3ab5165299a41c7be4eec98ed6a7f890
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-01 16:44:02 -0300

    Aula 3 - Atividade 01 Inserindo uma linha - INSERT

---
+++ Aula 3 - Atividade 01 Inserindo uma linha - INSERT.sql
@@ -0,0 +1,10 @@
+INSERT INTO tabelaclientes
+(id_cliente,
+nome_cliente,
+informacoes_de_contato,
+Endereço_Cliente)
+VALUES
+('1', 'Ana Silva', 'ana.silva@email.com', 'rua flores - casa 1');
+
+
+select * from tabelaclientes;
