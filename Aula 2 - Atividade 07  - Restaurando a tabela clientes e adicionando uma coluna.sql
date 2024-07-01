021d6f65cdb011d296d11b661da615da070ffb3a
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-01 16:42:52 -0300

    Restaurando a tabela clientes e adicionando uma coluna
    Update query

---
+++ Aula 2 - Atividade 07  - Restaurando a tabela clientes e adicionando uma coluna.sql
@@ -0,0 +1,12 @@
+-- Restaurando a tabela clientes
+
+CREATE TABLE tabelaclientes (
+  ID_Cliente INT PRIMARY KEY,
+  Nome_Cliente VARCHAR (250),
+  Informacoes_de_Contato VARCHAR (250)
+  );
+  
+
+
+
+ALTER TABLE tabelaclientes ADD Endereço_Cliente VARCHAR (250) -- incluido uma coluna na tabela
