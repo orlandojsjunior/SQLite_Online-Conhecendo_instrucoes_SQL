1a672a5a1a1072fad42009ec3ae5590bc25bd2f5
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-02 16:30:25 -0300

    Atualizando o salário dos funcionários do departamento de TI

---
+++ Exercicios Aula 5 - Atividade 05 - Mão na massa - aplicando na prática/06 - Atualizando o salário dos funcionários do departamento de  TI.sql
@@ -0,0 +1,7 @@
+-- 6) Atualize o salário dos funcionários do departamento de "TI" para 7500 na tabela funcionarios.
+
+UPDATE funcionarios SET salario = 7500 WHERE departamento = 'TI';
+
+
+
+SELECT * FROM funcionarios WHERE departamento = 'TI';
