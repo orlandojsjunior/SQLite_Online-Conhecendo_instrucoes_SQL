c415843a26fdd58af3798529a694bd19f09dc1f1
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-02 15:15:13 -0300

    Aplicando filtros - maior, menor, igual e diferente

---
+++ Aula 4 - Atividade 01 Aplicando filtros - maior, menor, igual e diferente.sql
@@ -0,0 +1,21 @@
+SELECT * FROM tabelapedidos WHERE total_do_pedido > 200;
+
+SELECT * FROM tabelapedidos WHERE total_do_pedido >= 200;
+
+
+SELECT * FROM tabelapedidos WHERE total_do_pedido <= 200;
+
+
+SELECT * FROM tabelapedidos WHERE total_do_pedido < 200;
+
+
+SELECT * FROM tabelapedidos WHERE total_do_pedido <> 200;
+
+
+SELECT * FROM tabelapedidos WHERE total_do_pedido = 200;
+
+
+SELECT * FROM tabelaclientes WHERE nome_cliente > 'C';
+
+
+SELECT * FROM tabelapedidos WHERE data_do_pedido > '2023-09-19';
