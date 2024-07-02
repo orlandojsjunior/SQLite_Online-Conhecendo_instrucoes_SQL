ced1f2da293e5e59414e718814ee351702e49cf7
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-02 16:00:02 -0300

    Utilizando ALIAS

---
+++ Aula 4 - Atividade 07 Utilizando ALIAS.sql
@@ -0,0 +1,5 @@
+SELECT * FROM tabelaclientes;
+
+SELECT informacoes_de_contato AS email_cliente FROM tabelaclientes;  
+
+/* Sempre que quiser que a coluna apareça com um apelido na consulta, você precisa colocar essa linha de código que escrevemos para criar o apelido para a coluna em uma certa consulta.
