da44df2bf00a1f98dc7e03f6d45eac69a1b95783
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-01 21:19:42 -0300

    Resolvendo exercicio
    Adicionando um novo funcionário à tabela

---
+++ Exercicios Aula 3 - Atividade 08 - Mão na massa - aplicando na prática/Adicionando um novo funcionário à tabela.sql
@@ -0,0 +1,14 @@
+INSERT INTO Funcionarios (
+ID, 
+Nome, 
+Cargo, 
+Departamento, 
+Data_de_Contratacao, Salario
+)
+VALUES (
+'32D', 
+'João Silva', 
+'Desenvolvedor de Software', 
+'TI', 
+'2023-10-24', 
+6000.00);
