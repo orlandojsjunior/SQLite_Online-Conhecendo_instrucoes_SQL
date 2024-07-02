290ebfb9df741a9fbb55d55685fe1320ac297a45
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-02 16:00:31 -0300

    Deletando os dados - DELETE

---
+++ Aula 5 - Atividade 03 Deletando os dados - DELETE.sql
@@ -0,0 +1,13 @@
+SELECT * FROM tabelafornecedores WHERE país_de_origem = 'Turquia';
+
+
+DELETE FROM tabelafornecedores WHERE país_de_origem = 'Turquia'; -- Usando a cláusula DELETE 
+
+-----------------------------------------
+
+SELECT * FROM tabelafornecedores;
+
+DELETE FROM tabelafornecedores WHERE id > 35; -- Deletar todos os fornecedores com ID maior que 35
+
+
+
