c38993fe95b1609709e24a81a971e03785bff525
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-02 15:59:45 -0300

    Ordenando os dados - ORDER BY

---
+++ Aula 4 - Atividade 05 Ordenando os dados - ORDER BY.sql
@@ -0,0 +1,13 @@
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY nome_do_produto;
+
+
+SELECT * FROM tabelaprodutos;
+
+
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY data_de_inclusao;
+
+
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY fornecedor;
+
+
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY fornecedor DESC;
