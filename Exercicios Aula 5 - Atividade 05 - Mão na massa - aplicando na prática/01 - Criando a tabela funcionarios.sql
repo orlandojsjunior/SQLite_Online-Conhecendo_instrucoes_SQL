04137e41ebb119f85e0c2f5a1b5832d4e5710b21
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-02 16:25:47 -0300

    Criando a tabela funcionarios

---
+++ Exercicios Aula 5 - Atividade 05 - Mão na massa - aplicando na prática/01 - Criando a tabela funcionarios.sql
@@ -0,0 +1,23 @@
+-- Criando a tabela
+CREATE TABLE funcionarios (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    departamento VARCHAR(100),
+    salario FLOAT
+); 
+
+SELECT * FROM funcionarios;
+
+
+-- Inserindo dados na tabela
+INSERT INTO funcionarios (id, nome, departamento, salario) VALUES
+(1, 'Heitor Vieira', 'Financeiro', 4959.22),
+(2, 'Daniel Campos', 'Vendas', 3884.44),
+(3, 'Luiza Dias', 'TI', 8205.78),
+(4, 'Davi Lucas Moraes', 'Financeiro', 8437.02),
+(5, 'Pietro Cavalcanti', 'TI', 4946.88),
+(6, 'Evelyn da Mata', 'Vendas', 5278.88),
+(7, 'Isabella Rocha', 'Marketing', 4006.03),
+(8, 'Sra. Manuela Azevedo', 'Vendas', 6101.88),
+(9, 'Brenda Cardoso', 'TI', 8853.34),
+(10, 'Danilo Souza', 'TI', 8242.14);
