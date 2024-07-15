eedc72e224447ac09af01ec91c66cb1f090b85af
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-14 21:09:48 -0300

    Consulta 1

---
+++ Desafio final do curso/03.01 - Consulta 1.sql
@@ -0,0 +1,23 @@
+-- Consulta 1: Executar consultas para verificar se os dados foram importados corretamente em todas as tabelas.
+
+
+SELECT * FROM alunos;
+
+
+SELECT * FROM professores;
+
+
+SELECT * FROM disciplinas;
+
+
+SELECT * FROM turmas;
+
+
+SELECT * FROM Notas;
+
+
+SELECT * FROM turma_disciplina;
+
+
+SELECT * FROM turma_alunos;
+
