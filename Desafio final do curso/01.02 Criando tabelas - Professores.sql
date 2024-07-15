370b78d0e0dcc64d00544407547919d7103b244e
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-14 21:05:29 -0300

    Criando tabelas - Professores

---
+++ Desafio final do curso/01.02 Criando tabelas - Professores.sql
@@ -0,0 +1,8 @@
+CREATE TABLE professores (
+  id_professor INT PRIMARY KEY,
+  nome_professor VARCHAR (255),
+  data_nascimento DATE,
+  genero TEXT,
+  telefone_contato INT,
+  email VARCHAR (50)
+  )
