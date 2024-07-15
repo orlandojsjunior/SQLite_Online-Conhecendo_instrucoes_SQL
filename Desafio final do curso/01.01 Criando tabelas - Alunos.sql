c1bf00e141084684ae072646cafb877718ed27c9
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-14 21:05:05 -0300

    Criando tabelas - Alunos

---
+++ Desafio final do curso/01.01 Criando tabelas - Alunos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE alunos (
+    ID_Aluno INT PRIMARY KEY,
+    Nome_Aluno VARCHAR (255),
+    Data_Nascimento DATE,
+    Genero TEXT (50),
+    Endereco VARCHAR (255),
+    Telefone_contato INT,
+    Email VARCHAR (255)
+);
