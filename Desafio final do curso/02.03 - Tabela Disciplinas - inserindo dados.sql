00793b76661de40b964aadfe9a0102b27caad250
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-14 21:08:34 -0300

    inserindo dados

---
+++ Desafio final do curso/02.03 - Tabela Disciplinas - inserindo dados.sql
@@ -0,0 +1,17 @@
+INSERT INTO disciplinas (
+    id_disciplina,
+    nome_disciplina,
+    descricao,
+    carga_horaria,
+    professor
+)
+VALUES
+    (1, 'Matemática', 'Estudo de conceitos matemáticos avançados', '60', '1'),
+    (2, 'História', 'História mundial e local', '45', '2'),
+    (3, 'Física', 'Princípios fundamentais da física', '60', '1'),
+    (4, 'Química', 'Estudo da química e suas aplicações', '45', '3'),
+    (5, 'Inglês', 'Aulas de inglês para iniciantes', '45', '4'),
+    (6, 'Artes', 'Exploração da criatividade artística', '30', '5');
+
+    
+    
