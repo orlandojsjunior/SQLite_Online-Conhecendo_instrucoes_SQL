01914153cb956326b381b80e054d64dbc4d9a19e
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-14 21:08:14 -0300

    Tabela Professores - inserindo dados

---
+++ Desafio final do curso/02.02 - Tabela Professores - inserindo dados.sql
@@ -0,0 +1,15 @@
+INSERT INTO professores (
+    id_professor, 
+    nome_professor, 
+    data_nascimento, 
+    genero, 
+    telefone_contato, 
+    email)
+VALUES 
+(1, 'Ana Oliveira', '1980-05-25', 'Feminino', '(11) 1234-5678', 'ana@email.com'),
+(2, 'Carlos Ferreira', '1975-09-01', 'Masculino', '(11) 2345-6789', 'carlos@email.com'),
+(3, 'Mariana Santos', '1982-03-01', 'Feminino', '(11) 3456-7890', 'mariana@email.com'),
+(4, 'Ricardo Silva', '1978-08-20', 'Masculino', '(11) 7890-1234', 'ricardo@email.com'),
+(5, 'Fernanda Lima', '1985-01-03', 'Feminino', '(11) 4567-8901', 'fernanda@email.com');
+
+
