791a7872ff8c1d82f8639a69a21fd318be4fd58a
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-14 21:08:45 -0300

    inserindo dados

---
+++ Desafio final do curso/02.04 - Tabela Turmas - inserindo dados.sql
@@ -0,0 +1,12 @@
+INSERT INTO turmas (
+    id_turma,
+    nome_turma,
+    ano_letivo,
+    professor
+)
+VALUES
+    (1, 'Turma A', 2023, 1),
+    (2, 'Turma B', 2023, 2),
+    (3, 'Turma C', 2023, 3),
+    (4, 'Turma D', 2023, 4),
+    (5, 'Turma E', 2023, 5);
